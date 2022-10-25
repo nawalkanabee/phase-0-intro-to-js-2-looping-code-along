83d6954ce400844e5878462678d73893e2df9b4f
nawalkanabee <nawalkanabee@gmail.com>
2022-10-25 22:19:29 +0300

    Done with lops

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, surprise) {
+    let arr8 = [];
+    for (let i = 0; i < array.length; i++) {
+      arr8.push(`Thank you, ${array[i]}, for the wonderful ${surprise} gift!`);
+    }
+    return arr8;
+  }
+  console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "birthday"));
+  
+  function countDown(x) {
+    while (X >= 0) {
+      console.log(X--);
+    }
+  }
+  countDown(6);
